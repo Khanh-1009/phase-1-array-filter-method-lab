011e81fbc13024f309804c61448d213d14f5e4bb
Khanh Tran <katetrannbkhanh@gmail.com>
2022-09-22 17:00:15 -0700

    commit tree 54ea759df5a5f1add9d0d99f71ba19c3fa61071e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(collection, driver) {
+    return collection.filter(name => name.toLowerCase() == driver.toLowerCase())
+    }
+    
+function fuzzyMatch(collection, driver) {
+    return collection.filter(name => name[0] == driver[0]);
+}
+
+function matchName(collection, driver){
+ 
+    return collection.filter(names => names.name.toLowerCase() == driver.toLowerCase());
+    
+}
